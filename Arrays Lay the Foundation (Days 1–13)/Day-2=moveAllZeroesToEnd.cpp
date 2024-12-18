ed94be6a0b8d78f929007d1ed4b85e14621a33a6
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-18 20:56:11 +0530

    Update Day-2=moveAllZeroesToEnd.cpp

--- Arrays Lay the Foundation (Days 1–13)/Day-2=moveAllZeroesToEnd.cpp
+++ Arrays Lay the Foundation (Days 1–13)/Day-2=moveAllZeroesToEnd.cpp
@@ -1,1 +1,39 @@
-// Day 2 Coding
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        int zeroPointer = 0;
+        for(int i = 0; i< arr.size(); i++){
+            if(arr[i] != 0){
+                swap(arr[zeroPointer], arr[i]);
+                zeroPointer++;
+            }
+        }
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.pushZerosToEnd(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
