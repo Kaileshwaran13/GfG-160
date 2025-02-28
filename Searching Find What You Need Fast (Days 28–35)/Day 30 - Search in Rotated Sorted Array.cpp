a88a74ddb530d6d2d078f0eac965385124cb9ec4
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-28 22:52:50 +0530

    Create Day 30 - Search in Rotated Sorted Array.cpp

---
+++ Searching Find What You Need Fast (Days 28–35)/Day 30 - Search in Rotated Sorted Array.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &arr, int key)
+    {
+        // complete the function here
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] == key)
+                return i;
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
